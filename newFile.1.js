4fb04ab4de12a5ef632accaa07859158a1ee5cc9
Admin <admin@Admins-iMac.local>
2024-10-30 17:32:44 +0700

    first commit

--- newFile.1.js
+++
@@ -1,8 +0,0 @@
-menuBtn.addEventListener("click", (e) => {
-    navlinks.classList.toggle("open");
-
-    const isOpen = navlinks.classList.contains("open");
-    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-
-
-});
